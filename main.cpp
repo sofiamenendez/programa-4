1f7387d73eb6ee49d7b2cf368bada710f6692f43
sofiamenendez <samenendez97@unitec.edu>
2016-02-20 16:21:47 -0600

    Added files via upload

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+/*
+Ingresar dos numeros y determinar cual es el numero mayor.
+*/
+int main()
+
+    return 0;
+}
